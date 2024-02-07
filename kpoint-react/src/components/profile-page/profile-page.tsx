16e05facb3f3d5281a43d98bcda5e01333fe1ac8
Denys Stepanov <denis307wfs@gmail.com>
2024-02-07 21:07:58 +0200

    FRONT: profile-page.tsx

---
+++ kpoint-react/src/components/profile-page/profile-page.tsx
@@ -0,0 +1,16 @@
+import { FC } from 'react';
+
+import { UserType } from '../../common/types/user/user';
+
+interface ProfilePageProps {
+  user: UserType,
+}
+
+const ProfilePage: FC = () => {
+
+  return (
+    <div></div>
+  );
+};
+
+export { ProfilePage };
