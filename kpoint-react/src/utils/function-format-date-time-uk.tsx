90e902d2d2759dd86abdb032e1214d73aa196c5b
nikozka15 <vshyrshova@gmail.com>
2024-02-25 15:51:51 +0000

    pom version 0.0.8, added test for function-format-date-time-uk, changed function-format-date-time-uk.tsx

--- kpoint-react/src/utils/function-format-date-time-uk.tsx
+++ kpoint-react/src/utils/function-format-date-time-uk.tsx
@@ -4,95 +4,51 @@ export function formatDateTimeUk(dateTime: string): string {
   const inputDate = new Date(dateTime);
   const timeDifferenceInSeconds = Math.floor((currentDate.getTime() - inputDate.getTime()) / 1000);
 
-  const roundToNearestMultiple = (value: number, multiple: number): number => {
-    return Math.floor(value / multiple) * multiple;
-  };
-
   if (timeDifferenceInSeconds < 60) {
     // Less than a minute
     return '1 секунду тому';
   }
 
-  const timeInMinutes = timeDifferenceInSeconds / 60;
+  const timeInMinutes = Math.floor(timeDifferenceInSeconds / 60);
 
-  if (timeInMinutes >= 1 && timeInMinutes <= 2) {
-    // A minute or two
-    return '1 хвилину тому';
+  if (timeInMinutes < 60){
+    return convertTimeToString(timeInMinutes, 'хвилину', 'хвилини', 'хвилин');
   }
 
-  if (timeInMinutes < 60) {
-    // Less than an hour
-    const minutesAgo = roundToNearestMultiple(Math.floor(timeInMinutes), 5);
-
-    return `${minutesAgo} хвилин тому`;
-  }
-
-  const timeInHours = timeInMinutes / 60;
-
-  if (timeInHours >= 1 && timeInHours <= 2) {
-    // An hour or two
-    return '1 година тому';
-  }
+  const timeInHours = Math.floor(timeInMinutes / 60);
 
   if (timeInHours < 24) {
-    // Less than a day
-    const hoursAgo = roundToNearestMultiple(Math.floor(timeInHours), 5);
-
-    return `${hoursAgo} годин тому`;
+    return convertTimeToString(timeInHours, 'година', 'години', 'годин');
   }
 
-  const timeInDays = timeInMinutes / 24;
-
-  if (timeInDays >= 1 && timeInDays <= 2) {
-    // A day or two
-    return '1 день тому';
-  }
+  const timeInDays = Math.floor(timeInHours / 24);
 
   if (timeInDays < 30) {
-    // Less than a month (30 days)
-    const daysAgo = roundToNearestMultiple(Math.floor(timeInDays), 5);
-
-    return `${daysAgo} днів тому`;
+    return convertTimeToString(timeInDays, 'день', 'дні', 'днів');
   }
 
-  const timeInMonths = timeInDays / 30;
-
-  if (timeInMonths >= 1 && timeInMonths <= 2) {
-    // A months or two
-    return '1 місяць тому';
-  }
+  const timeInMonths = Math.floor(timeInDays / 30);
 
   if (timeInMonths < 12) {
-    // Less than a year
-    const monthsAgo = roundToNearestMultiple(Math.floor(timeInMonths), 5);
-
-    return `${monthsAgo} місяців тому`;
+    return convertTimeToString(timeInMonths, 'місяць', 'місяці', 'місяців');
   }
 
-  const timeInYears = timeInMonths / 12;
+  const timeInYears = Math.floor(timeInMonths / 12);
 
-  if (timeInYears >= 1 && timeInYears <= 2) {
-    // A year or two
-    return '1 рік тому';
-  }
+  return convertTimeToString(timeInYears, 'рік', 'роки', 'років');
 
-  // Over a year
-  const yearsAgo = roundToNearestMultiple(Math.floor(timeInYears), 5);
-
-  return `${yearsAgo} років тому`;
 }
 
-function convertTimeToString(date: number, singleForm: string, pluralForm: string): string {
-  const roundToNearestMultiple = (value: number, multiple: number): number => {
-    return Math.floor(value / multiple) * multiple;
-  };
+function convertTimeToString(date: number, singleForm: string,secondForm: string, pluralForm: string): string {
 
-  if (date >= 1 && date <= 2) {
-    return `1 ${singleForm} тому`;
+  if (Number.parseInt(date.toString().charAt(date.toString().length-1)) === 1 && date !== 11) {
+    return `${date} ${singleForm} тому`;
   }
 
-  const timeAgo = roundToNearestMultiple(Math.floor(date), 5);
+  if (date < 5 || (date > 20 && Number.parseInt(date.toString().charAt(date.toString().length-1)) < 5)) {
+    return `${date} ${secondForm} тому`;
+  }
 
-  return `${timeAgo} ${pluralForm} тому`;
+  return `${date} ${pluralForm} тому`;
 
 }
